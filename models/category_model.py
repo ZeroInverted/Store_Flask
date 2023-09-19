8be4c3930836e8f8be56ebdac5d795f04321e7b5
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-20 02:47:18 +0300

    Update models

--- models/category_model.py
+++ models/category_model.py
@@ -1,4 +1,5 @@
 from database import db
+import json
 
 
 class Category(db.Model):
@@ -8,3 +9,16 @@ class Category(db.Model):
     name = db.Column(db.String, unique=True, index=True)
 
     products = db.relationship("Product", back_populates="category")
+
+    def get_all_categories():
+        return [Category.to_json(category) for category in Category.query.all()]
+
+    def add_category(category_name):
+        new_category = Category(name=category_name)
+        db.session.add(new_category)
+        db.session.commit()
+        db.session.refresh(new_category)
+        return Category.to_json(new_category)
+
+    def to_json(self):
+        return {"name": self.name}
